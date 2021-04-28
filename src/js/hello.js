7b7a8db4ebeee64fc1ec9673aa1101c3980b6a5f
jinyonghu <jinyonghu@vzhuo.com>
2021-04-28 11:49:26 +0800

    业务测试代码

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,5 +1,14 @@
 import '../css/hello.less'
-console.log('---webpack--- MY_ENV=', MY_ENV)
-for (let i = 0; i < 3; i++) {
-  console.log('---webpack--- i =', i)
+const env = MY_ENV
+console.log('---webpack--- MY_ENV=', env)
+
+const config = { name: 'jin xiao ke', age: 18, sex: 'gender' }
+
+const testLog = () => {
+  for (let i = 0; i < 3; i++) {
+    const info = { ...config, age: config.age + 1 }
+    console.log('---webpack--- i =', info)
+  }
 }
+
+testLog()
